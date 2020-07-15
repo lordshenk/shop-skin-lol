9e1e3fc6f159d8ab006558a124da62991cd5e3ed
s1mple <duykhanh29091999@gmail.com>
2020-07-15 17:01:05 +0700

    deploy

--- src/components/home-component/Champions.js
+++ src/components/home-component/Champions.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Card, CardImg, CardBody, CardTitle } from 'reactstrap';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import Lazyload from 'react-lazyload';
