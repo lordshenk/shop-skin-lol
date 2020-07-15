752cd5215f1847939892a9d3279fea9bf30df888
s1mple <duykhanh29091999@gmail.com>
2020-07-15 16:54:37 +0700

    fix pagination

--- src/components/home-component/Champions.js
+++ src/components/home-component/Champions.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardImg, CardBody, CardTitle } from 'reactstrap';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import Lazyload from 'react-lazyload';
-import champions from '../../assets/champions.js';
+import championsData from '../../assets/champions.js';
 import lazyImg from '../../assets/lazy-img.png';
 import Pagination from 'react-js-pagination';
 
 function Champions(props) {
+	const champions = championsData;
 	const [ championsFilter, setChampionsFilter ] = useState([]);
-	function handleInput(e) {
-		if ( e.key === 'Enter') {
-			let inputValue = e.target.value.toLowerCase();
-			let filter = champions.filter( i => Object.keys(i)[0].toLowerCase().includes(inputValue));
-			setChampionsFilter(filter);
-		}		
-	}
-
 	const [ activePage, setActivePage] = useState(1);
   const history = useHistory();
   let page = useParams().page;
   page = page ? Number(page) : 1;
-  const pageChampions = champions.slice((page - 1)*20, page*20);
+  let pageChampions = championsFilter.length > 0 ? championsFilter.slice((page - 1)*20, page*20) : champions.slice((page - 1)*20, page*20) ;
+	function handleInput(e) {
+		if ( e.key === 'Enter') {
+			let inputValue = e.target.value.toLowerCase();
+			let filter = champions.filter( i => Object.keys(i)[0].toLowerCase().includes(inputValue));
+			setActivePage(1);
+    	history.replace(`/home/1`);
+			setChampionsFilter(filter);
+		}		
+	}
   function handlePageChange(pageNumber) {
     setActivePage(pageNumber);
-    history.push(`/home/${pageNumber}`);
+    history.replace(`/home/${pageNumber}`);
   }
 
 	return (
@@ -31,25 +33,12 @@ function Champions(props) {
 			<h1>Champions</h1><br/>
 			<input type="text" placeholder='search champion' className='text-center' onKeyPress={handleInput}/>
 			<div className='d-flex flex-wrap'>
-				{championsFilter.length === 0 ? pageChampions.map(( ele, ind) => (
+				{pageChampions.map(( ele, ind) => (
 					<div key={ind} className='champion d-flex justify-content-center mb-3 mb-md-5'>
 						<Card 
 							tag={Link} to={`/champion/${Object.keys(ele)[0].split(' ').join('')}`}
 							className='justify-content-center align-items-center'
 							title={Object.keys(ele)[0]}>
-							<Lazyload
-								height={200} offset={100} once={true}
-								placeholder={<CardImg top width='100%' src={lazyImg} alt='champion' />}>
-								<CardImg top width='100%' src={ele[Object.keys(ele)[0]].img[0]} alt='champion' />
-							</Lazyload>
-							<CardBody>
-								<CardTitle>{Object.keys(ele)[0]}</CardTitle>
-							</CardBody>
-						</Card>
-					</div>
-				)) : championsFilter.map(( ele, ind) => (
-					<div key={ind} className='champion d-flex justify-content-center mb-3 mb-md-5'>
-						<Card tag={Link} to={`/champion/${Object.keys(ele)[0].split(' ').join('')}`} className='justify-content-center align-items-center'>
 							<Lazyload
 								height={200} offset={100} once={true}
 								placeholder={<CardImg top width='100%' src={lazyImg} alt='champion' />}>
@@ -66,7 +55,7 @@ function Champions(props) {
         hideFirstLastPages
         activePage={activePage}
         itemsCountPerPage={20}
-        totalItemsCount={148}
+        totalItemsCount={championsFilter.length ? championsFilter.length : champions.length}
         onChange={handlePageChange.bind(this)}
       />
 		</div>
